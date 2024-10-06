43e74964b9d89c7f8df2b55f8d7a60acf200a6ad
karoljaro <111643413+karoljaro@users.noreply.github.com>
2024-10-06 17:48:11 +0200

    feat: Add Tailwind CSS, Vuex store, Google Fonts, and ESLint to project
    Added Tailwind CSS, Vuex store with Pinia, Google Fonts, and ESLint to the project for styling, state management, font imports, and code quality. Updated Nuxt configuration and package.json scripts accordingly.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  pages: true,
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/google-fonts',
+    '@nuxt/eslint'
+  ]
 })
