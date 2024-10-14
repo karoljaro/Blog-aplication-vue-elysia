7ec9c3b183468fb2f4e97b5ca5b0f41aea91249c
karoljaro <111643413+karoljaro@users.noreply.github.com>
2024-10-15 01:03:35 +0200

    feat: Add ContentBox component and FutureTech section
    Added a new ContentBox component for sharing content and 
    implemented a new FutureTech section with detailed features.

---
+++ frontend/components/shared/ContentBox.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-dark/10 border border-dark/15 rounded-xl">
+        <slot />
+    </div>
+</template>
---
+++ frontend/components/shared/SectionLabel.vue
@@ -0,0 +1,7 @@
+<template>
+   <div class="rounded bg-dark/15 text-center sm:px-2 sm:py-1 xl:px-2.5 xl:py-1.5 max-w-max">
+      <p class="text-left font-inter font-medium !leading-normal -tracking-[3%] text-white sm:text-sm lg:text-base xl:text-xl">
+         <slot />
+      </p>
+   </div>
+</template>
