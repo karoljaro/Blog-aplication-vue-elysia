fba6aa88213dc42c32c29cd8f0e227d492dae952
karoljaro <111643413+karoljaro@users.noreply.github.com>
2024-10-15 02:49:22 +0200

    refactor: Update Section Header component structure and style.

---
+++ frontend/components/shared/SectionHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-full border-y border-y-dark/15 bg-dark/10">
+         <div class="mx-auto max-w-screen-xl sm:space-y-2.5 sm:px-4 sm:py-10 lg:px-20 lg:py-20 xl:space-y-4 xl:px-40.5 xl:py-30">
+            <slot />
+         </div>
+      </div>
+</template>
